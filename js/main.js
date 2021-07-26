6a30808bd4c477ca6d27dd1ae06790e25e75faab
Alexandr-77 <62809713+Alexandr-77@users.noreply.github.com>
2021-07-26 22:11:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const vis = document.querySelector('.menu');
+const miClass = document.querySelector('.burger__block');
+    
+    miClass.addEventListener('click', miClick);
+
+
+function miClick() {
+    if (vis.style.display === 'flex'){
+       vis.style.display = 'none';
+   } else {
+       vis.style.display = 'flex';
+   }
+} 
+
+// ===================================
+
+const content = document.querySelector('.list-drop');
+const clak = document.querySelector('.menu-item-2');
+    
+    clak.addEventListener('click', mifunction);
+
+
+function mifunction() {
+    if (content.style.display === 'block'){
+       content.style.display = 'none';
+   } else {
+       content.style.display = 'block';
+   }
+} 
